b57c21feedc9627ca833d8d3bd3818f0918b20c4
Chris J. Karr <chris@audacious-software.com>
2019-07-08 13:12:57 +0000

    Adding support for data source queries

--- pdk_client/client.py
+++ pdk_client/client.py
@@ -69,6 +69,9 @@ class PDKClient(object):
 
         return PDKDataPointQuery(self.token, self.site_url, **kwargs).filter(recorded__lte=now)
 
+    def query_data_sources(self, *args, **kwargs): # pylint: disable=unused-argument
+        return PDKDataSourceQuery(self.token, self.site_url, **kwargs).exclude(pk=None)
+
 
 class PDKDataPointQuery(object): # pylint: disable=too-many-instance-attributes
     def __init__(self, token, site_url, *args, **kwargs): # pylint: disable=unused-argument
@@ -216,3 +219,141 @@ class DatetimeEncoder(json.JSONEncoder):
             return super(DatetimeEncoder, obj).default(obj)
         except TypeError:
             return str(obj)
+
+
+class PDKDataSourceQuery(object): # pylint: disable=too-many-instance-attributes
+    def __init__(self, token, site_url, *args, **kwargs): # pylint: disable=unused-argument
+        self.token = token
+        self.site_url = site_url
+
+        page_size = PDK_API_DEFAULT_PAGE_SIZE
+
+        if 'page_size' in kwargs:
+            page_size = kwargs['page_size']
+            del kwargs['page_size']
+
+        self.page_size = page_size
+
+        self.filters = []
+        self.excludes = []
+        self.order_bys = []
+
+        if kwargs:
+            self.filters.append(kwargs)
+
+        self.total_count = None
+        self.page_index = 0
+        self.current_index = 0
+
+        self.current_page = None
+
+    def filter(self, **kwargs):
+        query = PDKDataSourceQuery(self.token, self.site_url, self.page_size)
+
+        query.filters = list(self.filters)
+        query.excludes = list(self.excludes)
+        query.order_bys = list(self.order_bys)
+
+        query.filters.append(kwargs)
+
+        return query
+
+    def exclude(self, **kwargs):
+        query = PDKDataSourceQuery(self.token, self.site_url, self.page_size)
+
+        query.filters = list(self.filters)
+        query.excludes = list(self.excludes)
+        query.order_bys = list(self.order_bys)
+
+        query.excludes.append(kwargs)
+
+        return query
+
+    def order_by(self, *args):
+        query = PDKDataSourceQuery(self.token, self.site_url, self.page_size)
+
+        query.filters = list(self.filters)
+        query.excludes = list(self.excludes)
+        query.order_bys = list(self.order_bys)
+
+        query.order_bys.append(args)
+
+        return query
+
+    def count(self):
+        if self.total_count is not None:
+            return self.total_count
+
+        self.load_page(0)
+
+        return self.total_count
+
+    def __iter__(self):
+        self.load_page(0)
+
+        return self
+
+    def next(self):
+        if self.current_index >= self.total_count:
+            raise StopIteration
+
+        if self.current_index >= (self.page_index + 1) * self.page_size:
+            self.load_page(self.page_index + 1)
+
+        value = self.current_page[self.current_index % self.page_size]
+
+        self.current_index += 1
+
+        return value
+
+    def __getitem__(self, slice_item):
+        if isinstance(slice_item, (int, long)):
+            index = slice_item
+
+            if self.current_page is None:
+                self.load_page(0)
+
+            if index < 0:
+                index = self.total_count + index
+
+            if (index >= (self.page_index * self.page_size)) and (index < ((self.page_index + 1) * self.page_size)):
+                return self.current_page[index % self.page_size]
+
+            self.load_page(int(index / self.page_size))
+
+            return self.current_page[index % self.page_size]
+        elif isinstance(slice_item, slice):
+            eprint('SLICE NOT YET SUPPORTED: ' + str(slice_item))
+
+        return []
+
+    def first(self):
+        return self[0]
+
+    def last(self):
+        return self[-1]
+
+    def load_page(self, page_number):
+        self.page_index = page_number
+
+        payload = {
+            'token': self.token,
+            'page_size': self.page_size,
+            'page_index': self.page_index,
+            'filters': json.dumps(self.filters, cls=DatetimeEncoder),
+            'excludes': json.dumps(self.excludes, cls=DatetimeEncoder),
+            'order_by': json.dumps(self.order_bys, cls=DatetimeEncoder),
+        }
+
+        fetch_page = requests.post(self.site_url + '/api/data-sources.json', data=payload)
+
+        if fetch_page.status_code == requests.codes.ok:
+            response_payload = fetch_page.json()
+
+            self.total_count = response_payload['count']
+            self.page_index = response_payload['page_index']
+            self.page_size = response_payload['page_size']
+
+            self.current_page = response_payload['matches']
+        else:
+            fetch_page.raise_for_status()
